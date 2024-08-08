b804a2eab5a69bfb3945a3f3875de36de3bfc9a7
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-08-08 20:58:15 +0530

    Add email-user-data with view-user-data and upload-user-data

--- Front-end/src/app/whatsapp-module/Services/braces-transform.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BracesTransformPipe } from './braces-transform.pipe';
-
-describe('BracesTransformPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BracesTransformPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
