a9f4797ace6ca9cb07c3f9108fec39f3b5ad37c6
Jashany <jsingh12_be22@thapar.edu>
2024-09-02 00:05:09 +0530

    Added Auth model and Template model

---
+++ back-end/Models/Template.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const variable = {
+  name: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  value: {
+    type: String,
+  },
+};
+
+const TemplateSchema = new mongoose.Schema({
+  templateId: {
+    type: String,
+    unique: true,
+  },
+  Subject: {
+    type: String,
+  },
+  Content: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["email", "sms", "whatsapp"],
+    required: true,
+  },
+  variables: [variable],
+});
+
+const Template = mongoose.model("Template", TemplateSchema);
+
+export default Template;
