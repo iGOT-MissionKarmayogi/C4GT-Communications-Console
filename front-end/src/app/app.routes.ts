e49bbb3562a32c5dc5411adc5ec29bc5b6e508f1
AAKARSH LOHANI <134588187+Aakarsh-Lohani@users.noreply.github.com>
2024-09-09 21:59:29 +0530

    Update app.routes.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+
 export const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login',component: HomeComponent},
