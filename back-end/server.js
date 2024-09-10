95658ff9c4dbd4ce5ac76c1257d57cf162b1f627
Sagar Kori <sagarkoriup11@gmail.com>
2024-09-10 12:46:19 +0530

    Added controller logics for templates and send message

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import ConnectDB from './DB/index.js';
 import { errorHandler,notfound } from './middlewares/error.js';
-import AuthRouter from './Routes/Auth.routes.js';
-import TemplateRouter from './Routes/Template.routes.js';
-import userRouter from './Routes/User.routes.js';
-// "start": "concurrently \"nodemon server.js\" \"nodemon Email/backend/server.js\""
+
+//Email-Specific
+import getEmailService from './email-backend/config/emailConfig.js';
+
+
 
 // Import routes
+import AuthRouter from './Routes/Auth.routes.js';
+import TemplateRouter from './Routes/Template.routes.js';
+import EmailRouter from './email-backend/routes/index.js';
 import WhatsappRouter from './Routes/Whatsapp.routes.js';
+import userRouter from './Routes/User.routes.js';
+import listEndpoints from 'express-list-endpoints';
+
 // Initialize express
 const app = express();
 
@@ -20,7 +27,7 @@ dotenv.config();
 
 // Middlewares
 app.get('/', (req, res) => {
-    res.send("Welcome to Notifier project")
+    res.send("Welcome to Back-end of Notifier project")
 })
 
 app.use(express.json());
@@ -32,15 +39,23 @@ app.use(cors({
 app.use(bodyParser.json());
 
 
+//Email-Specific
+app.use(bodyParser.urlencoded({ extended: true }));
+const emailService = getEmailService();
+
 // Routes
+app.use('/api/auth', AuthRouter);
+app.use('/api/templates', TemplateRouter);
+app.use('/api/email', EmailRouter);
 app.use('/api/whatsapp',WhatsappRouter);
+app.use('/api/user',userRouter);
 
 
 // Error handling middlewares
 app.use(notfound);
 app.use(errorHandler);
 
-
+console.log("All endpoints are :",listEndpoints(app));
 // Connect to MongoDB
 ConnectDB().then(() => {
     app.listen(process.env.PORT, () => {
